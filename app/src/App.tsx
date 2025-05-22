868038b4ded3ba47e1fee5b09603db3fc8ee2f9a
Pinkaew Horputra <pinoripin@gmail.com>
2025-05-22 03:06:45 -0400

    fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,27 @@
 import Footer from './pages/HomePage/Footer';
 import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
 
+function loadGoogleMapsScript(apiKey: string) {
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
+  script.async = true;
+  document.body.appendChild(script);
+}
 
 function App() {
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+    if (apiKey) {
+      loadGoogleMapsScript(apiKey);
+    }
+  }, []);
 
   return (
     <>
       <Outlet></Outlet>
-      <Footer />      
+      <Footer />
     </>
   );
 }
